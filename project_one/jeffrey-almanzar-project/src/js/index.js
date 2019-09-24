78a3cb648f218ab0d572d94c764c52eadc9bd952
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2019-09-24 19:31:25 -0400

    complete project prototype

--- project_one/jeffrey-almanzar-project/src/js/index.js
+++ project_one/jeffrey-almanzar-project/src/js/index.js
@@ -1,5 +1,4 @@
-// Elements
-const INPUT_FIELD = document.getElementById("hex-input");
+
 const SUBMIT_BUTTON = document.getElementById("hex-submit");
 
 const HEX = {
@@ -39,6 +38,65 @@ function hexToBinary(hexValue) {
     result += HEX[hexValue[i]] + " ";
   }
   return result;
+}
+
+function hexToDecimal(hexValue) {
+  hexValue = hexValue
+    .split("")
+    .reverse()
+    .join("");
+  const HEX = {
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    a: 10,
+    b: 11,
+    c: 12,
+    d: 13,
+    e: 14,
+    f: 15
+  };
+  let result = 0;
+  for (let i = 0; i < hexValue.length; i++) {
+    result += HEX[hexValue[i]] * Math.pow(16, i);
+  }
+
+  return result;
+}
+
+function hexToDottedDecimal(hexValue) {
+  const hexValues = insertDashIntoString(hexValue).split("-");
+
+  let result = "";
+  hexValues.forEach((element, index) => {
+    if (index != 0 && index != hexValues.length) {
+      result += "." + hexToDecimal(element);
+      console.log(index);
+    } else {
+      result += hexToDecimal(element);
+    }
+  });
+  return result;
+}
+
+function insertDashIntoString(hexValue) {
+  let newHexValue = "";
+  const insertDash = { 2: true, 4: true, 6: true };
+
+  hexValue.split("").forEach((element, index) => {
+    if (insertDash[index]) {
+      newHexValue += "-" + element;
+    } else {
+      newHexValue += element;
+    }
+  });
+  return newHexValue;
 }
 
 function getIpClass(binaryValue) {
@@ -119,15 +177,21 @@ function displayInfo(ipClass, networkId, hostId, decimalDottedNotation) {
   RESULTS.style.display = "block";
 }
 
+function clear() {
+  const RESULTS = document.getElementById("result-container");
+  const INNER_RESULT = document.getElementById("inner-result-container");
+  INNER_RESULT.innerHTML = "";
+}
 SUBMIT_BUTTON.onclick = e => {
+  const INPUT_FIELD = document.getElementById("hex-input");
   const userHexValue = INPUT_FIELD.value.toLowerCase();
-
+  clear();
   if (validateInput(userHexValue)) {
     const binaryValue = hexToBinary(userHexValue);
     const ipClass = getIpClass(binaryValue);
     const networkId = getNetworkID(userHexValue, ipClass);
     const hostId = getHostID(userHexValue, ipClass);
-    const decimalDottedNotation = "temporal";
+    const decimalDottedNotation = hexToDottedDecimal(userHexValue);
     displayInfo(ipClass, networkId, hostId, decimalDottedNotation);
   } else {
     alert("NO VALID INPUT!");
