9318bb88915b71e8bbbc713dbba7ed6808dc51e4
miguelgomez2 <47229737+miguelgomez2@users.noreply.github.com>
2019-10-16 00:05:34 -0400

    Add files via upload

--- project_one/script.js
+++ project_one/script.js
@@ -2,17 +2,16 @@ var button1 = document.getElementById("grading-element-one");
 var link = document.getElementById("grading-element-two");
 var button2 = document.getElementById("grading-element-three");
 var span = document.getElementById("grading-element-three-consequence");
-var paragraph = document.getElementById("grading-element-one-consequence")
+var ul = document.getElementById("grading-element-one-consequence")
 
 
 
 button1.addEventListener("click", function() {
-	paragraph.innerHTML = 'grading-element-one-consequence';
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode("testing"));
+	ul.appendChild(li);
 })
 
-link.addEventListener("click", function(){
-	paragraph.innerHTML = 'grading-element-one-consequence';
-})
 
 button2.addEventListener("click", function() {
 	span.innerHTML = "hows it going?";
