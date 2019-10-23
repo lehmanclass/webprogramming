0b4c8b7be53d3f1f9f01917fbe50e896cfd91b91
miguelgomez2 <47229737+miguelgomez2@users.noreply.github.com>
2019-10-22 23:56:14 -0400

    fixes

--- project_one/script.js
+++ project_one/script.js
@@ -2,17 +2,23 @@ var button1 = document.getElementById("grading-element-one");
 var link = document.getElementById("grading-element-two");
 var button2 = document.getElementById("grading-element-three");
 var span = document.getElementById("grading-element-three-consequence");
-var ul = document.getElementById("grading-element-one-consequence")
+var div = document.getElementById("uptop");
+
+
 
 
 
 button1.addEventListener("click", function() {
+	var ul = document.createElement("ul");
+	ul.id = "grading-element-one-consequence";
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode("testing"));
 	ul.appendChild(li);
-})
+	div.appendChild(ul);
+
+	}, {once : true});
 
 
 button2.addEventListener("click", function() {
-	span.innerHTML = "hows it going?";
+	document.getElementById("grading-element-three-consequence").style.visibility = "visible";
 })
