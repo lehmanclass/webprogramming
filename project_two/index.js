789076d06cca45cde3320b0fdb25977e3898fe49
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-10-24 18:56:21 -0400

    add test three

--- project_two/index.js
+++ project_two/index.js
@@ -15,13 +15,24 @@ app.get("/test_one", (req, res) => {
 
 app.post("/test_two", (req, res) => {
   const { fruit, cake } = req.body;
-  res.json({ massage: { fruitMessage: `i love to eat ${fruit} with ${cake}` } });
+  res.json({
+    massage: { fruitMessage: `i love to eat ${fruit} with ${cake}` }
+  });
 });
 
-app.get("/test_three/", (req, res) => {});
+app.get("/test_three/:fruit/:cake", (req, res) => {
+  const { fruit, cake } = req.params;
+  isAuthorized(req.headers)
+    ? res.json({ message: `you sent ${fruit} and ${cake}, but I only eat ${cake}!`})
+    : res.json({ message: "unauthorized" });
+});
 
 app.post("/test_four", (req, res) => {});
 
 app.put("/test_five/write", (req, res) => {});
 
 app.listen(3000, () => console.log("server is running"));
+
+function isAuthorized(headers) {
+  return headers.authorization && headers.authorization === "Bearer projecttwo";
+}
