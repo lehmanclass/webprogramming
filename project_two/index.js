4d97cc74fb12f1d7728165b82ecde997d813d8aa
jasmineeuy <jeuyoquea@gmail.com>
2019-10-30 23:03:30 -0400

    Added parts of test four , db and body-parser

--- project_two/index.js
+++ project_two/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const db = require(`./db.js`);
+
 const app = express();
+
+
+
 const PORT = 3000;
 
+const TOKEN = 'projecttwo'
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+const DATABASE = new FakeAssDatabase();
+const DATABASE ={};
 
 
 
@@ -34,7 +47,65 @@ app.post('/test_two', (req,res) => {
 });
 
 
-``
+app.get('/test_three/:afruit/:acake',(req,res) => {
+
+    const token = req.headers.authorization.replace('Bearer ', '');
+
+
+    const acake = req.params.acake;
+    const afruit = req.params.afruit;
+
+    
+
+    if (TOKEN !== token) {
+        return res.json({ message: 'unauthorized' });
+
+    }
+
+    if(TOKEN === token){
+
+        return res.json({ message : `you sent ${afruit} and ${acake}, but I only eat ${acake}!`})
+    }
+
+
+});
+
+
+app.post('/test_four',(res,req) =>{
+
+    const afruit = req.body.fruit;
+    const cake = req.body.cake; 
+
+
+
+
+    res.json({ message: `i am getting really sick of eating ${afruit} after filling up on ${cake}` });
+
+
+});
+
+
+
+app.put('test_five/write',(res,req) => {
+
+});
+
+app.get('test_five/read',(res,req) => {
+
+});
+
+app.get('test_five/read?fruit=${aSecondFruit}&cake=${aCake}',(res,req) => {
+
+});
+
+
+
+
+
+
+
+
+
 
 
 app.listen(PORT,() => console.log(`listening on port ${PORT}`));
