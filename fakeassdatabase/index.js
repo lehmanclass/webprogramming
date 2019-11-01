0d22cd9a9ef03507ebf397ddeefdc3678cf8c394
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2019-10-31 21:05:34 -0400

    moved project into project_2 folder

--- fakeassdatabase/index.js
+++ fakeassdatabase/index.js
@@ -1,94 +1,39 @@
-var express = require('express');
-var FakeAssDatabase = require('./db.js') ;
-var app = express(); 
+class FakeAssDatabase {
 
-const DATABASE = new FakeAssDatabase(); 
+	constructor() {
+		this.data = {};
+	}
 
-const PORT = 3000;
-const TOKEN = 'projecttwo';
+	create(keyString, valueString) {
+		if (this.data[keyString]) {
+			throw new Error(`${keyString} already exists!`);
+		}
+		this.data[keyString] = valueString;
+	}
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+	read(keyString) {
+		return this.data[keyString];
+	}
 
+	update(keyString, valueString) {
+		if (!this.data[keyString]) {
+			throw new Error(`${keyString} does not exist!`);
+		}
+		this.data[keyString] = valueString;
+	}
 
-//Test ONE:
-app.get('/test_one', function (req, res) {
-  const { fruit, cake } = req.query
-  
-  res.json({
-    message: {
+	delete(keyString) {
+		if (!this.data[keyString]) {
+			throw new Error(`${keyString} does not exist!`);
+		}
+		delete this.data[keyString];
+	}
 
-      fruit,
-      cake
-    }
-  })
-});
-
-//Test TWO://
-app.post('/test_two',(req, res) => {
-  const { fruit, cake } = req.body
-
-  return res.json({
-     message: `i love to eat ${fruit} with ${cake}`});
-
-});
-
-//Test THREE://
-app.get('/test_three/:fruit/:cake',(req, res) => {
-
-  const {fruit, cake} = req.params;
-
-  console.log(req.headers.authorization);
-  const token = req.headers.authorization.replace('Bearer ', '');
-  //const token = req.headers.authorization;
-  
-  if(TOKEN === token) {
-    return res.json({message:`you sent ${fruit} and ${cake}, but I only eat ${cake}!`});
-  }
-
-  res.json({ message: "unauthorized" });
-});
-
-//Test FOUR://
-app.post ('/test_four', (req,res) => {
-  const {fruit, cake} = req.body;
-
-  return res.json({
-    message: `i am getting really sick of eating ${fruit} after filling up on ${cake}`});
-
-});
+	dump() {
+		return JSON.stringify(this.data);
+	}
 
 
-//Test FIVE://
-app.put('/test_five/write', (req,res) => {
-  const {fruit, cake} = req.body;
-  
-  var readDatabaseFruit = DATABASE.read(fruit);
-  if(!readDatabaseFruit) {
-    DATABASE.create(fruit, 1);
-  } else {
-    DATABASE.update(fruit, readDatabaseFruit + 1);
-  }
+}
 
-  var readDatabaseCake = DATABASE.read(cake);
-  if(!readDatabaseCake) {
-    DATABASE.create(cake, 1);
-
-  } else {
-    DATABASE.update(cake, readDatabaseCake + 1);
-  }
-  res.json({ message: `you sent ${fruit} and ${cake}` });
-
-
-});
-
-app.get('/test_five/read',(req, res) => {
-  res.json(JSON.parse(DATABASE.dump()));
-
-});
-
-
-
-app.listen(PORT, () => console.log(`I AM LISTENING on ${PORT}`));
-console.log('Server started! At http://localhost:' + PORT);
-
+module.exports = FakeAssDatabase ;
