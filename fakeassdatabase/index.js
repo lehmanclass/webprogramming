81c2d22ac31cf21e7ead6a4e17a3d0b020180dfe
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2019-10-31 20:52:16 -0400

    completed project 2

--- fakeassdatabase/index.js
+++ fakeassdatabase/index.js
@@ -1,37 +1,94 @@
-class FakeAssDatabase {
+var express = require('express');
+var FakeAssDatabase = require('./db.js') ;
+var app = express(); 
 
-	constructor() {
-		this.data = {};
-	}
+const DATABASE = new FakeAssDatabase(); 
 
-	create(keyString, valueString) { // OOPS - the value should be a number
-		if (this.data[keyString]) {
-			throw new Error(`${keyString} already exists!`);
-		}
-		this.data[keyString] = valueString;
-	}
+const PORT = 3000;
+const TOKEN = 'projecttwo';
 
-	read(keyString) {
-		return this.data[keyString];
-	}
-
-	update(keyString, valueString) { // public service announcement - value is a number
-		if (!this.data[keyString]) {
-			throw new Error(`${keyString} does not exist!`);
-		}
-		this.data[keyString] = valueString;
-	}
-
-	delete(keyString) {
-		if (!this.data[keyString]) {
-			throw new Error(`${keyString} does not exist!`);
-		}
-		delete this.data[keyString];
-	}
-
-	dump() {
-		return JSON.stringify(this.data);
-	}
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 
-}
+//Test ONE:
+app.get('/test_one', function (req, res) {
+  const { fruit, cake } = req.query
+  
+  res.json({
+    message: {
+
+      fruit,
+      cake
+    }
+  })
+});
+
+//Test TWO://
+app.post('/test_two',(req, res) => {
+  const { fruit, cake } = req.body
+
+  return res.json({
+     message: `i love to eat ${fruit} with ${cake}`});
+
+});
+
+//Test THREE://
+app.get('/test_three/:fruit/:cake',(req, res) => {
+
+  const {fruit, cake} = req.params;
+
+  console.log(req.headers.authorization);
+  const token = req.headers.authorization.replace('Bearer ', '');
+  //const token = req.headers.authorization;
+  
+  if(TOKEN === token) {
+    return res.json({message:`you sent ${fruit} and ${cake}, but I only eat ${cake}!`});
+  }
+
+  res.json({ message: "unauthorized" });
+});
+
+//Test FOUR://
+app.post ('/test_four', (req,res) => {
+  const {fruit, cake} = req.body;
+
+  return res.json({
+    message: `i am getting really sick of eating ${fruit} after filling up on ${cake}`});
+
+});
+
+
+//Test FIVE://
+app.put('/test_five/write', (req,res) => {
+  const {fruit, cake} = req.body;
+  
+  var readDatabaseFruit = DATABASE.read(fruit);
+  if(!readDatabaseFruit) {
+    DATABASE.create(fruit, 1);
+  } else {
+    DATABASE.update(fruit, readDatabaseFruit + 1);
+  }
+
+  var readDatabaseCake = DATABASE.read(cake);
+  if(!readDatabaseCake) {
+    DATABASE.create(cake, 1);
+
+  } else {
+    DATABASE.update(cake, readDatabaseCake + 1);
+  }
+  res.json({ message: `you sent ${fruit} and ${cake}` });
+
+
+});
+
+app.get('/test_five/read',(req, res) => {
+  res.json(JSON.parse(DATABASE.dump()));
+
+});
+
+
+
+app.listen(PORT, () => console.log(`I AM LISTENING on ${PORT}`));
+console.log('Server started! At http://localhost:' + PORT);
+
