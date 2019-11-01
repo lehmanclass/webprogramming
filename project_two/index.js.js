62a02f1a9eb7720f765472beeb5f77796e066c16
Mahbobamim <mahbubamim18@gmil.com>
2019-10-31 23:15:24 -0400

    new branch commit

--- project_two/index.js.js
+++ project_two/index.js.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 const database= require('./database');
-const fakedata= new  database();
+const DATABASE= new  database();
 
 app.get('/test_one',(req, res)=> {
    const {fruit, cake}=req.query;
@@ -90,31 +90,75 @@ app.post('/test_two',(req, res)=> {
     });
 
         
-    app.put('/test_five/write',(req, res)=> {
-    
-        let n = 0;
-       
-     
-        const aFruit= req.body.fruit;
-       const aCake=req.body.cake;
-         
 
 
-      fakedata.create(aFruit, n+1) 
-       fakedata.create(aCake, n+1)
-
- 
-       return res.json( `{ "message": {"You sent ${aFruit} and ${aCake}" }}`)
- 
-     });
-     app.get('/test_five/read',(req, res)=> {
-       
-       
-        res.json(fakedata.data);
-         
-        return res.json( `{ "message": {"You sent ${aFruit} : ${number} and ${aCake}" }}`)
-
-     });
+     app.put('/test_five/write', (req,res) => {
+        const fruit = req.body.fruit;
+        const cake = req.body.cake;
+        
+        try{
+        
+        DATABASE.create(fruit, 1);
+        }
+        catch{
+        DATABASE.update(fruit, DATABASE.read(fruit) + 1);
+        
+        }
+        try{
+        
+        DATABASE.create(cake, 1);
+        }
+        catch{
+        DATABASE.update(cake, DATABASE.read(cake) + 1);
+        
+        }
+        res.json({
+        message: ' you sent ' + fruit  + ' and ' + cake
+        })
+        
+        })
+        
+        app.get('/test_five/read', (req,res) => {
+        const fruit = req.body.fruit;
+        const cake = req.body.cake;
+        
+        res.json(
+        DATABASE.data
+        )
+        })
+        
+        app.put('/test_five/write', (req,res) => {
+        const fruit = req.body.fruit;
+        const cake = req.body.cake;
+        
+        res.json({ message: 'i am getting really sick of eating ' +  fruit  + ' after filling up on ' + cake
+        });
+        
+        })
+        
+        app.get('/test_five/read/:fruit/:cake', (req,res) => {
+        const fruit = req.body.fruit;
+        const cake = req.body.cake;
+        try{
+        
+        DATABASE.create(fruit, 1);
+        }
+        catch{
+        DATABASE.update(fruit, fruit + 1);
+        
+        }
+        try{
+        
+        DATABASE.create(cake, 1);
+        }
+        catch{
+        DATABASE.update(cake, 1);
+        
+        }
+        res.json(
+        DATABASE.data
+        )
+        })
      
 
 
