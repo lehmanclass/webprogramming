e42e664f880d91443e043df3b7ccf16b6b8a7b75
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-11-05 18:40:12 -0500

    fix test two and use eslint

--- project_two/index.js
+++ project_two/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const request = require("request");
+const express = require('express');
+const request = require('request');
 
-const helpers = require("./utils");
-const fakeDB = require("../fakeassdatabase/index");
+const helpers = require('./utils');
+const fakeDB = require('../fakeassdatabase/index');
 
 const DB = new fakeDB();
 
@@ -11,54 +11,54 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get("/test_one", (req, res) => {
-  const message = {
-    message: {
-      fruit: req.query.fruit,
-      cake: req.query.cake
-    }
-  };
-  res.json(message);
+app.get('/test_one', (req, res) => {
+	const message = {
+		message: {
+			fruit: req.query.fruit,
+			cake: req.query.cake
+		}
+	};
+	res.json(message);
 });
 
-app.post("/test_two", (req, res) => {
-  const { fruit, cake } = req.body;
-  res.json({ massage: `i love to eat ${fruit} with ${cake}` });
+app.post('/test_two', (req, res) => {
+	const { fruit, cake } = req.body;
+	res.json({ message: `i love to eat ${fruit} with ${cake}` });
 });
 
-app.get("/test_three/:fruit/:cake", (req, res) => {
-  const { fruit, cake } = req.params;
-  helpers.isAuthorized(req.headers)
-    ? res.json({
-        message: `you sent ${fruit} and ${cake}, but I only eat ${cake}!`
-      })
-    : res.json({ message: "unauthorized" });
+app.get('/test_three/:fruit/:cake', (req, res) => {
+	const { fruit, cake } = req.params;
+	helpers.isAuthorized(req.headers)
+		? res.json({
+			message: `you sent ${fruit} and ${cake}, but I only eat ${cake}!`
+		})
+		: res.json({ message: 'unauthorized' });
 });
 
-app.post("/test_four", (req, res) => {
-  const { fruit, cake } = req.body;
-  res.json({
-    message: `i am getting really sick of eating ${fruit} after filling up on ${cake}`
-  });
+app.post('/test_four', (req, res) => {
+	const { fruit, cake } = req.body;
+	res.json({
+		message: `i am getting really sick of eating ${fruit} after filling up on ${cake}`
+	});
 });
 
-app.put("/test_five/write", (req, res) => {
-  const { fruit, cake } = req.body;
-  helpers.handleUpdateRequest(fruit, DB);
-  helpers.handleUpdateRequest(cake, DB);
-  res.json({ message: `you sent ${fruit} and ${cake}` });
+app.put('/test_five/write', (req, res) => {
+	const { fruit, cake } = req.body;
+	helpers.handleUpdateRequest(fruit, DB);
+	helpers.handleUpdateRequest(cake, DB);
+	res.json({ message: `you sent ${fruit} and ${cake}` });
 });
 
-app.get("/test_five/read", (req, res) => {
-  res.json(DB.data);
+app.get('/test_five/read', (req, res) => {
+	res.json(DB.data);
 });
 
-app.get("/external_api", (req, res) => {
-  request("https://jsonplaceholder.typicode.com/posts", function(error, response, body) {
-    const data = JSON.parse(body); 
-    // If I don't parse the data first it's not sending json when res.json(data); for some reason.
-    res.json(data);
-  });
+app.get('/external_api', (req, res) => {
+	request('https://jsonplaceholder.typicode.com/posts', function(error, response, body) {
+		const data = JSON.parse(body); 
+		// If I don't parse the data first it's not sending json when res.json(data); for some reason.
+		res.json(data);
+	});
 });
 
-app.listen(3000, () => console.log("server is running"));
+app.listen(3000, () => console.log('server is running'));
