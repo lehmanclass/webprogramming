cbf308ec75fe212c72a3e40565b16a09f933c9fe
Mahbobamim <mahbubamim18@gmil.com>
2019-11-05 19:04:06 -0500

    preparing to pull from upstream

--- test_project_two/index.js
+++ test_project_two/index.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 const database= require('./database');
-const DATABASE= new  database();
+const fakedatabase= new  database();
 
 app.get('/test_one',(req, res)=> {
    const {fruit, cake}=req.query;
@@ -98,18 +98,19 @@ app.post('/test_two',(req, res)=> {
         
         try{
         
-        DATABASE.create(fruit, 1);
+            fakedatabase.create(fruit, 1);
         }
         catch{
-        DATABASE.update(fruit, DATABASE.read(fruit) + 1);
+            fakedatabase.update(fruit,  fakedatabase.read(fruit) + 1);
+            
         
         }
         try{
         
-        DATABASE.create(cake, 1);
+            fakedatabase.create(cake, 1);
         }
         catch{
-        DATABASE.update(cake, DATABASE.read(cake) + 1);
+            fakedatabase.update(cake,  fakedatabase.read(cake) + 1);
         
         }
         res.json({
@@ -123,42 +124,11 @@ app.post('/test_two',(req, res)=> {
         const cake = req.body.cake;
         
         res.json(
-        DATABASE.data
+            fakedatabase.data
         )
         })
         
-        app.put('/test_five/write', (req,res) => {
-        const fruit = req.body.fruit;
-        const cake = req.body.cake;
-        
-        res.json({ message: 'i am getting really sick of eating ' +  fruit  + ' after filling up on ' + cake
-        });
-        
-        })
-        
-        app.get('/test_five/read/:fruit/:cake', (req,res) => {
-        const fruit = req.body.fruit;
-        const cake = req.body.cake;
-        try{
-        
-        DATABASE.create(fruit, 1);
-        }
-        catch{
-        DATABASE.update(fruit, fruit + 1);
-        
-        }
-        try{
-        
-        DATABASE.create(cake, 1);
-        }
-        catch{
-        DATABASE.update(cake, 1);
-        
-        }
-        res.json(
-        DATABASE.data
-        )
-        })
+      
      
 
 
