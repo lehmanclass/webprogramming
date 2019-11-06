2b6be103693392cc70ca24177fb904608ece6313
Ramish Saqib <48165537+RamishSaqib@users.noreply.github.com>
2019-11-05 21:36:37 -0500

    project_two resubmitting

---
+++ project_two/index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const FakeAssDatabase = require('./db.js'); 
+const app = express(); 
+const PORT = 3000;
+
+const DATABASE = new FakeAssDatabase; 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); 
+
+app.get(`/test_one`, (req, res) => {
+    const { fruit, cake } = req.query; 
+
+    res.json({
+        message: {
+            fruit,
+            cake
+        }
+    });
+});
+
+app.post('/test_two', (req, res) => {
+    const {fruit, cake} = req.body;
+
+    res.json({
+        message: `i love to eat ${fruit} with ${cake}`
+    });
+});
+
+
+app.get('/test_three/:fruit/:cake', (req, res) => {
+    const {fruit, cake} = req.params;
+
+    if (req.headers.authorization !== "Bearer projecttwo") {
+        return res.json({ message: "unauthorized" });
+    }
+
+    res.json({
+        message: `you sent ${fruit} and ${cake}, but I only eat ${cake}!`
+    });
+
+});
+
+app.post('/test_four', (req, res) => {
+    const{fruit, cake} = req.body;
+
+    res.json({ message: `i am getting really sick of eating ${fruit} after filling up on ${cake}` });
+
+});
+
+
+app.put('/test_five/write', (req, res) => {
+    const {fruit, cake} = req.body;
+
+    const x = DATABASE.read(fruit); 
+    if (!x) { 
+        DATABASE.create(fruit, 1); 
+    } else {
+        DATABASE.update(fruit, x+1); 
+    }
+
+    const y = DATABASE.read(cake);
+    if (!y) {
+        DATABASE.create(cake, 1);
+    } else {
+        DATABASE.update(cake, y+1);
+    }
+
+    return res.json({
+        message : `you sent ${fruit} and ${cake}`
+    });
+});
+
+app.get('/test_five/read', (req, res) => {
+    return res.json(JSON.parse(DATABASE.dump()));
+});
+
+app.get('/db/read', (req, res) => {
+    res.json(DATABASE.data);
+});
+
+app.get('/db/write', (req, res) => {
+    console.log(req.query);
+    DATABASE.create(req.query.key, req.query.value);
+    res.json(DATABASE);
+});
+
+app.listen(PORT, () => console.log('I am listening'));
