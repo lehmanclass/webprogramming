6f129333d5229dc33ff3ce2fb8b7fc96eaf8b879
Shany <lajarashany@gmail.com>
2019-11-20 22:13:20 -0500

    update

---
+++ project_3/build/precache-manifest.a199f7502e9f1fc54be981ccb93bb7f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b0aa7a525c9e5759c477e226c4e2f20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71d3d7014e6158f5aa07",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b6efb51fd751ed187407",
+    "url": "/static/js/2.2b472183.chunk.js"
+  },
+  {
+    "revision": "71d3d7014e6158f5aa07",
+    "url": "/static/js/main.e5b596b9.chunk.js"
+  },
+  {
+    "revision": "894e5083db5193c905a0",
+    "url": "/static/js/runtime-main.1798709a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
