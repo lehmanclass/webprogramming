3fc05381750dbb4c365b7a759817dec7b938e233
Christopher Estevez <CodingWithCDJE@gmail.com>
2019-11-21 22:13:39 -0500

    Fix a line of Code

--- project_three/helpers.js
+++ project_three/helpers.js
@@ -1,4 +1,4 @@
-const apiKey = require('./env');
+const apiKey = process.env.GIPHY_API_TOKEN || require('./env');
 
 function getAPI (searchTerm) {
     return `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}&limit=5`;
