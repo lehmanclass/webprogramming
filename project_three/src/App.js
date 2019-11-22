9311ca1d58935fbdd2cc36e7e3ec958c7243c6b9
Ralphie Perez <ironware6@gmail.com>
2019-11-21 21:48:30 -0500

    Dogs Rule

--- project_three/src/App.js
+++ project_three/src/App.js
@@ -1,49 +1,68 @@
 import React from 'react';
 import Axios from 'axios';
- 
+import './index.css';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-      this.state = {
-        results : [] ,
-        searchTerm : ''
-      };
+    this.state = {
+      results: [],
+      searchTerm: '',
+      focusImg: false
+    };
   }
 
   handleClick = () => {
     const { searchTerm } = this.state;
     Axios.post(`http://localhost:3000/gif_search`, {
-        searchTerm
+      searchTerm
     })
-    .then(res => {this.setState({results : res.data.results})});
+      .then(res => { this.setState({ results: res.data.results }) });
 
   }
 
   updateThis = (event) => {
-    this.setState({searchTerm : event.target.value})
+    this.setState({ searchTerm: event.target.value })
   }
 
-  render(){
-    const { results } = this.state;
+  focusThis = (focusImg) => {
+    // document.getElementsByClassName('gif-focus-display').focus();
+    this.setState({ focusImg }); //Concaatenate each gif imagine as an index and place into the array list results. Should be able to iterate through.
+  }
+
+  render() {
+    const { results, focusImg } = this.state;
+
+    if (focusImg) {
+      return (
+        <img
+          className="gif-focus-display"
+          src={focusImg.gifUrl}
+          alt="gif"
+        />
+      )
+    }
 
     return (
       <React.Fragment> {/*Better than a div*/}
-        <h2> Search Dat Giphy (NO CATS THEY SUCK) </h2>
+        <div id="search">
+          <input type="text" id="gif-search-input" onChange={this.updateThis} ></input>
 
-        <input type = "text" id = "gif-search-input" onChange = {this.updateThis} ></input>
+          <span>
+            <button id="gif-search-submit" onClick={this.handleClick}>Search</button>
+          </span>
 
-        <span>
-          <button id = "gif-search-submit" onClick = {this.handleClick}>Search</button>
-        </span>
+        </div>
+
+        <h1> Search the GiF of your choice :D </h1>
 
         <div>
-          {results.map((result,i) => (<img className="gif-result-display" src={result.gifUrl} key={i} alt = "gif"/>))}
+          {results.map((result, i) => (<img className="gif-result-display" src={result.gifUrl} key={i} alt="gif" onClick={() => this.focusThis(result)} />))}
         </div>
       </React.Fragment>
     );
   }
-  
+
 }
 
 
