ea9b103283bd5023b7d29630219628ca1d33f986
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-12-02 21:24:27 -0500

    define routes

--- project_four/index.js
+++ project_four/index.js
@@ -1,12 +1,51 @@
-const express = require('express');
-
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-   res.send('Hello')
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
+
+app.post("/login", (req, res) => {
+
+});
+
+app.post("/register", (req, res) => {
+
+});
+
+app.post("/createGoal", (req, res) => {
+
+});
+
+app.get("/goals", (req, res) => {
+
+});
+
+app.delete("/goals/id", (req, res) => {
+
+});
+
+app.put('/goals/id', (req, res) => {
+
+})
+
+app.post("/createTask", (req, res) => {
+
+});
+
+app.get("/tasks", (req, res) => {
+
+});
+
+app.delete("/tasks/id", (req, res) => {
+
+});
+
+app.put('/goals/id', (req, res) => {
+    
+})
 
 app.listen(5000, () => console.log("server is running"));
