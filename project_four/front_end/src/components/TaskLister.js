2fdee430b253d18de8437891dec60b9c8fd2da5a
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-12-04 06:35:59 -0500

    create task card prototype

--- project_four/front_end/src/components/TaskLister.js
+++ project_four/front_end/src/components/TaskLister.js
@@ -1,14 +1,16 @@
 import React from "react";
-import Nav from './Nav';
+import Nav from "./Nav";
+import TaskCard from "./TaskCard";
 
 class TaskLister extends React.Component {
   render() {
     return (
-        <div>
-          <Nav />
-          <h1>TaskLister</h1>
-        </div>
-      );
+      <div>
+        <Nav />
+        <h1>TaskLister</h1>
+        <TaskCard title="Do Exercise" status="No started" />
+      </div>
+    );
   }
 }
 
