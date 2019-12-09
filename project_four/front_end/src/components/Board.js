460a04cd059691c56671ddf736fb527d321ffc2e
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-12-08 19:21:49 -0500

    created board prototype

--- project_four/front_end/src/components/Board.js
+++ project_four/front_end/src/components/Board.js
@@ -1,12 +1,31 @@
 import React from "react";
-import Nav from './Nav';
+import Nav from "./Nav";
 
 class Board extends React.Component {
   render() {
     return (
       <div>
-        <Nav logout={this.props.logout}/>
+        <Nav logout={this.props.logout} />
         <h1>Board</h1>
+        <div className="board-column-container">
+          <div className="board-column-item">
+            <h3 className="column-title">Not started</h3>
+            <div className="board-column-inner-item"></div>
+          </div>
+
+          <div className="board-column-item">
+            <h3 className="column-title">In progress</h3>
+            <div className="board-column-inner-item"></div>
+          </div>
+          <div className="board-column-item">
+            <h3 className="column-title">On hold</h3>
+            <div className="board-column-inner-item"></div>
+          </div>
+          <div className="board-column-item">
+            <h3 className="column-title">Done</h3>
+            <div className="board-column-inner-item"></div>
+          </div>
+        </div>
       </div>
     );
   }
