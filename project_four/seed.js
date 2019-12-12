35bcfeedc196c2051e9e20b9910d6a0d4260fe3b
Ramish Saqib <48165537+RamishSaqib@users.noreply.github.com>
2019-12-12 16:34:15 -0500

    Project four final

---
+++ project_four/seed.js
@@ -0,0 +1,45 @@
+const pg = require('pg');
+
+console.log('SEED IS RUNNING');
+
+const create = `
+	CREATE TABLE tic (
+		id serial PRIMARY KEY,
+		task text,
+		completed boolean
+	);
+`;
+
+const insert = `
+	INSERT INTO tic (task, completed) VALUES ('go to the store', false);
+`;
+
+const db = new pg.Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'postgres',
+  port: 3000,
+});
+
+db.connect();
+
+db.query(create)
+	.then(({ rows = [] }) => {
+		console.log('create ran successfully');
+		return rows;
+	})
+	.catch(err => {
+		console.error(err);
+		console.error(err.stack);
+	});
+
+db.query(insert)
+	.then(({ rows = [] }) => {
+		console.log('insert ran successfully');
+		return rows;
+	})
+	.catch(err => {
+		console.error(err);
+		console.error(err.stack);
+	});
